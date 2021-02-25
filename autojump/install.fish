9b68df3c82dbb0e7a95ba957cb4898bed699e46a
reishou <reishou90@gmail.com>
2021-02-25 15:52:53 +0700

    add tmux, autojump, ssh

---
+++ autojump/install.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+curl -sL https://raw.githubusercontent.com/wting/autojump/master/bin/autojump.fish -o ~/.config/fish/conf.d/autojump.fish
--- ssh/install.fish
+++ ssh/install.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
-ln -sf "$HOME/Documents/.ssh" "$HOME/.ssh"
+ln -sf "$HOME"/Documents/.ssh "$HOME"
+
+chmod 400 "$HOME"/.ssh/id_rsa_*
+chmod 400 "$HOME"/.ssh/*.pem
+chmod 644 "$HOME"/.ssh/id_rsa_*.pub

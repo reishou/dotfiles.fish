5f739419c917195cb6eff7658e9cbd00a9fc28fa
reishou <reishou90@gmail.com>
2021-02-27 21:11:00 +0700

    remove set chmod ssh

--- ssh/install.fish
+++ ssh/install.fish
@@ -2,6 +2,6 @@
 
 ln -sf "$HOME"/Documents/.ssh "$HOME"
 
-chmod 400 "$HOME"/.ssh/id_rsa_*
-chmod 400 "$HOME"/.ssh/*.pem
-chmod 644 "$HOME"/.ssh/id_rsa_*.pub
+#chmod 400 "$HOME"/.ssh/id_rsa_*
+#chmod 400 "$HOME"/.ssh/*.pem
+#chmod 644 "$HOME"/.ssh/id_rsa_*.pub
